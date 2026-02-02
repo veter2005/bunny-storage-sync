00ce163f465a45438006c2698c2b2db2469769d1
Viktar Mukha <viktar.mukha@veraxen.com>
2026-02-02 15:46:44 +0300

    Add size-only and only-missing flags

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 
 func main() {
 	var dryRun bool
+	var sizeOnly bool
+	var onlyMissing bool
 	flag.BoolVar(&dryRun, "dry-run", false, "Show the difference and exit")
+	flag.BoolVar(&sizeOnly, "size-only", false, "Use only file size for comparison instead of checksum")
+	flag.BoolVar(&onlyMissing, "only-missing", false, "Only upload missing files, do not update existing ones")
 	flag.Parse()
+
 	src := flag.Arg(0)
 	zoneName := flag.Arg(1)
 	apiKey := os.Getenv("BCDN_APIKEY")
@@ -20,9 +25,16 @@ func main() {
 		fmt.Println("API key must be set with BCDN_APIKEY")
 		os.Exit(-1)
 	}
+
 	fmt.Println("Walking path:", src)
 	storage := api.BCDNStorage{ZoneName: zoneName, APIKey: apiKey}
-	syncerService := syncer.BCDNSyncer{storage, dryRun}
+	syncerService := syncer.BCDNSyncer{
+		API:         storage,
+		DryRun:      dryRun,
+		SizeOnly:    sizeOnly,
+		OnlyMissing: onlyMissing,
+	}
+
 	err := syncerService.Sync(src)
 	fmt.Println(err)
 }
